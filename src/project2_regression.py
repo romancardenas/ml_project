cab7ae5aa7c0f634a6f56f68a13ef40493ed6ac0
Román Cárdenas <rcardenas.rod@gmail.com>
2018-11-01 11:46:50 +0100

    boxplot and outlier removal. Images

--- src/project2_regression.py
+++ src/project2_regression.py
@@ -2,4 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, KFold
 
-data = pd.read_csv('../data/kc_house_data_clean.csv')
+data = pd.read_csv('../data/kc_house_data_clean_regression.csv')
+attributeNames = list(data)[2:]
+print(attributeNames)
