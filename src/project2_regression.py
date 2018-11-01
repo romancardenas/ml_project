b45e7d340c9266129d843de8492bce93afc5b1c0
Román Cárdenas <rcardenas.rod@gmail.com>
2018-11-01 16:10:24 +0100

    keep working on it...

--- src/project2_regression.py
+++ src/project2_regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import KFold
 import sklearn.linear_model as lm
 from matplotlib.pyplot import figure, plot, subplot, title, xlabel, ylabel, show, clim
 from toolbox_02450 import feature_selector_lr, bmplot
@@ -13,50 +13,63 @@ X = data.values[:, 1:]
 y = data.values[:, 0]
 N, M = X.shape
 
-## Crossvalidation
-# Create crossvalidation partition for evaluation
-K = 5
+#################################################################################
+#                                                                               #
+#                             2-LAYER CROSS-VALIDATION                          #
+#                                                                               #
+#################################################################################
+K = 5  # Outer cross-validation fold: 5-Fold
 CV = KFold(n_splits=K, shuffle=True)
 
-# Initialize variables
-Features = np.zeros((M, K))
-Error_train = np.empty((K, 1))
-Error_test = np.empty((K, 1))
-Error_train_fs = np.empty((K, 1))
-Error_test_fs = np.empty((K, 1))
-Error_train_nofeatures = np.empty((K, 1))
-Error_test_nofeatures = np.empty((K, 1))
+# Initialize variables for simple base case (just predicting always the mean)
+Error_train_nofeatures = np.empty((K, 1))   # Train error (base case)
+Error_test_nofeatures = np.empty((K, 1))    # Test error (base case)
 
+# Initialize variables for linear regression
+LR_Error_train = np.empty((K, 1))           # Train error (all features)
+LR_Error_test = np.empty((K, 1))            # Test error (all features)
+LR_Features = np.zeros((M, K))              # For keeping track of selected features
+LR_Error_train_fs = np.empty((K, 1))        # Train error (feature selection)
+LR_Error_test_fs = np.empty((K, 1))         # Test error (feature selection)
+
+
+
+##################################################################################
+#                                                                                #
+#                                  LINEAR REGRESSION                             #
+#                                                                                #
+##################################################################################
+print("\n")
+print('STARTING WITH LINEAR REGRESSION MODEL')
 k = 0
-for train_index, test_index in CV.split(X):
+for train_index, test_index in CV.split(X): # Outer 2-layer cross-validation loop
     # extract training and test set for current CV fold
     X_train = X[train_index, :]
     y_train = y[train_index]
     X_test = X[test_index, :]
     y_test = y[test_index]
-    internal_cross_validation = 10
 
-    # Compute squared error without using the input data at all
+    # BASE CASE (JUST PREDICTING THE MEAN)
     Error_train_nofeatures[k] = np.square(y_train - y_train.mean()).sum() / y_train.shape[0]
     Error_test_nofeatures[k] = np.square(y_test - y_test.mean()).sum() / y_test.shape[0]
 
-    # Compute squared error with all features selected (no feature selection)
+    # USING ALL THE FEATURES
     m = lm.LinearRegression(fit_intercept=True, normalize=True).fit(X_train, y_train)
-    Error_train[k] = np.square(y_train - m.predict(X_train)).sum() / y_train.shape[0]
-    Error_test[k] = np.square(y_test - m.predict(X_test)).sum() / y_test.shape[0]
+    LR_Error_train[k] = np.square(y_train - m.predict(X_train)).sum() / y_train.shape[0]
+    LR_Error_test[k] = np.square(y_test - m.predict(X_test)).sum() / y_test.shape[0]
 
-    # Compute squared error with feature subset selection
-    textout = 'verbose'
-    selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, internal_cross_validation,
-                                                                          display=textout)
-    Features[selected_features, k] = 1
-    # .. alternatively you could use module sklearn.feature_selection
+    # USING FEATURE SELECTION
+    K_internal = 10
+    # textout = 'verbose'
+    textout = ''
+    selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, K_internal, display=textout)
+    LR_Features[selected_features, k] = 1
     if len(selected_features) is 0:
         print('No features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).')
     else:
         m = lm.LinearRegression(fit_intercept=True, normalize=True).fit(X_train[:, selected_features], y_train)
-        Error_train_fs[k] = np.square(y_train - m.predict(X_train[:, selected_features])).sum() / y_train.shape[0]
-        Error_test_fs[k] = np.square(y_test - m.predict(X_test[:, selected_features])).sum() / y_test.shape[0]
+        LR_Error_train_fs[k] = np.square(y_train - m.predict(X_train[:, selected_features])).sum() / y_train.shape[0]
+        LR_Error_test_fs[k] = np.square(y_test - m.predict(X_test[:, selected_features])).sum() / y_test.shape[0]
 
         figure(k)
         subplot(1, 2, 1)
@@ -69,35 +82,69 @@ for train_index, test_index in CV.split(X):
         clim(-1.5, 0)
         xlabel('Iteration')
         show()
-
     print('Cross validation fold {0}/{1}'.format(k + 1, K))
     print('Train indices: {0}'.format(train_index))
     print('Test indices: {0}'.format(test_index))
     print('Features no: {0}\n'.format(selected_features.size))
-
     k += 1
-# Display results
+print('FINISHING WITH LINEAR REGRESSION MODEL')
+print("\n")
+
+##################################################################################
+#                                                                                #
+#                                   DISPLAY RESULTS                              #
+#                                                                                #
+##################################################################################
+print('##################################################################################')
+print('#                                                                                #')
+print('#                                   DISPLAY RESULTS                              #')
+print('#                                                                                #')
+print('##################################################################################')
+print('------------------------------- LINEAR REGRESSION --------------------------------')
 print('\n')
-print('Computing just the mean:\n')
+print('Computing just the mean (base case):')
 print('- Training error: {0}'.format(Error_train_nofeatures.mean()))
 print('- Test error:     {0}'.format(Error_test_nofeatures.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_nofeatures.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_nofeatures.sum())/Error_test_nofeatures.sum()))
-print('Linear regression without feature selection:\n')
-print('- Training error: {0}'.format(Error_train.mean()))
-print('- Test error:     {0}'.format(Error_test.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
-print('Linear regression with feature selection:\n')
-print('- Training error: {0}'.format(Error_train_fs.mean()))
-print('- Test error:     {0}'.format(Error_test_fs.mean()))
-print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
-print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
+print('\n')
+print('Linear regression without feature selection:')
+print('- Training error: {0}'.format(LR_Error_train.mean()))
+print('- Test error:     {0}'.format(LR_Error_test.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum() - LR_Error_train.sum()) / Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum() - LR_Error_test.sum()) / Error_test_nofeatures.sum()))
+print('\n')
+print('Linear regression with feature selection:')
+print('- Training error: {0}'.format(LR_Error_train_fs.mean()))
+print('- Test error:     {0}'.format(LR_Error_test_fs.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum() - LR_Error_train_fs.sum()) / Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum() - LR_Error_test_fs.sum()) / Error_test_nofeatures.sum()))
 
 figure(k)
 subplot(1, 3, 2)
-bmplot(attributeNames, range(1, Features.shape[1]+1), -Features)
+bmplot(attributeNames, range(1, LR_Features.shape[1] + 1), -LR_Features)
 clim(-1.5, 0)
 xlabel('Crossvalidation fold')
 ylabel('Attribute')
 show()
+
+# Inspect selected feature coefficients effect on the entire dataset and
+# plot the fitted model residual error as function of each attribute to
+# inspect for systematic structure in the residual
+# TODO revisar esto... Las etiquetas no cuadran
+f = 2  # cross-validation fold to inspect
+ff = LR_Features[:, f - 1].nonzero()[0]
+if len(ff) is 0:
+    print('\nNo features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).')
+else:
+    m = lm.LinearRegression(fit_intercept=True, normalize=True).fit(X[:, ff], y)
+
+    y_est = m.predict(X[:, ff])
+    residual = y - y_est
+
+    figure(k + 1, figsize=(12, 6))
+    title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
+    for i in range(0, len(ff)):
+        subplot(2, np.ceil(len(ff) / 2.0), i + 1)
+        plot(X[:, ff[i]], residual, '.')
+        xlabel(attributeNames[ff[i]])
+        ylabel('residual error')
+    show()
