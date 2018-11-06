25a495317403ede92ff8e567b38d75540af15788
Matt Parker <matthewp14@gmail.com>
2018-11-06 17:46:28 +0100

    finished 2-layer cv for all models

--- src/project2_classification.py
+++ src/project2_classification.py
@@ -13,64 +13,90 @@ from scipy.stats import mode
 from matplotlib.pyplot import figure, plot, xlabel, ylabel, legend, show, boxplot
 from scipy.io import loadmat
 from sklearn import model_selection, tree
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import MultinomialNB
 
 
 
-data = pd.read_csv('../data/kc_house_data_clean_regularzip.csv')
+data = pd.read_csv('../data/kc_house_data_clean_regression_zip.csv')
 
 dataObjectNames = np.array(range(0,7045))
 
-attributeNames = ['price','bedrooms','bathrooms','sqft_living','sqft_lot','floors','condition','sqft_above','sqft_basement']
+attributeNames = ['bedrooms','bathrooms','sqft_living','sqft_lot','floors','condition','sqft_above','sqft_basement']
 
-X = np.asarray(data[['price','bedrooms','bathrooms','sqft_living','sqft_lot','floors','condition','sqft_above','sqft_basement']])
+X = np.asarray(data[['bedrooms','bathrooms','sqft_living','sqft_lot','floors','condition','sqft_above','sqft_basement']])
 
-allCNames = np.array(data['zipcode'])
+#X = StandardScaler().fit_transform(X)
 
-classNames = np.unique((data['zipcode']))
+allCNames = np.array(data['price'])
+allCNames.sort()
 
-cnTemp = classNames.tolist()
+low = 4574
+med = 9167
 
-y = np.asarray(list(range(0,7045)))
-for x in range(7045):
-    y[x]=cnTemp.index(allCNames[x])
+priceCategories = list(range(0,13724))
+i = 0
+
+for x in allCNames:
+    if x < 350000:
+        priceCategories[i] = 'low'
+    elif x < 650000:
+        priceCategories[i] = 'med'
+    else :
+        priceCategories[i] = 'high'
+    i+=1
+    
+classNames = ['low','med','high']
+
+y = np.asarray(list(range(0,13724)))
+for x in range(13724):
+    y[x] = classNames.index(priceCategories[x])
+
 
 N, M = X.shape
 C = len(classNames)
+
+
 
 
 ###NOTE: In order to save time I just created 3 tc variables, uncomment the appropriate one 
 ### depending on which parameter you wish to control
 
 #depth as complexity controlling
-tc = np.arange(2, 21, 1)
+tc = np.arange(2, 10, 1)
 name = 'depth'
-
-# min_samples_leaf as complexity controlling
-#tc = np.arange(1,10,1)
-#name = 'min_leaf_samples'
-
-# impurity gain
-#tc =np.arange(0,21,1)
-#name = 'min_impurity_gain'
-
-
 
 # K-fold crossvalidation
 K = 10
 CV = model_selection.KFold(n_splits=K,shuffle=True)
 
 # Initialize variable
-Error_train = np.empty((K,1))
-Error_test = np.empty((K,1))
-optimal_depth = np.empty((K,1))
+dtc_Error_train = np.empty((K,1))
+dtc_Error_test = np.empty((K,1))
+dtc_optimal_depth = np.empty((K,1))
 
 k=0
 col = 0;
 row = 0;
+L = 10
+alpha = 1.0
 
-f, ax = plt.subplots(4,3)
+
+# Initialize variable
+knn_Error_test = np.empty((K,1))
+knn_optimal_N = np.empty((K,1))
+
+
+#NB Errors
+#NB_Error_test = np.empty((K,1))
+#AllErrors = [[range(0,K+1)][range(0,7)]
+#AllErrors[0,:] = ['DTC TEST ERROR', 'DTC TRAIN ERROR', 'Depth', 'KNN ERROR', 'Neighbors', 'NB ERROR', 'Prior']
+
+
+#f, ax = plt.subplots(4,3)
 for train_index, test_index in CV.split(X):
     print('Computing CV fold: {0}/{1}..'.format(k+1,K))
 
@@ -81,9 +107,12 @@ for train_index, test_index in CV.split(X):
 
     K2 = 10
     CV2 = model_selection.KFold(n_splits=K,shuffle=True)
-    inner_error_train = np.empty((len(tc), K2))
-    inner_error_test = np.empty((len(tc), K2))
+    dtc_inner_error_train = np.empty((len(tc), K2))
+    dtc_inner_error_test = np.zeros((len(tc), K2))
     k2=0
+    knn_errors = np.empty((K2,L))
+    
+    nb_errors = np.empty((K2,2))
     
 
     for dtrain_index, dval_index in CV2.split(X_train):
@@ -92,6 +121,14 @@ for train_index, test_index in CV.split(X):
         dtrainx, dtrainy = X_train[dtrain_index,:], y[dtrain_index]
         dtestx, dtesty = X_train[dval_index,:], y[dval_index]
         
+        
+        for l in range(1,L+1):
+            knclassifier = KNeighborsClassifier(n_neighbors=l);
+            knclassifier.fit(dtrainx, dtrainy);
+            y_est = knclassifier.predict(dtestx);
+            knn_errors[k2,l-1] = np.sum(y_est!=dtesty)/len(y_est)
+
+        
         for i, t in enumerate(tc):
              dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth = t)
              dtc = dtc.fit(dtrainx,dtrainy.ravel())
@@ -99,64 +136,107 @@ for train_index, test_index in CV.split(X):
              y_est_train = dtc.predict(dtrainx)
              misclass_rate_test = sum((y_est_test!=dtesty)) / float(len(y_est_test))
              misclass_rate_train = sum((y_est_train!= dtrainy)) /float(len(y_est_train))
-             inner_error_test[i,k2], inner_error_train[i,k2] = misclass_rate_test, misclass_rate_train
+             dtc_inner_error_test[i,k2], dtc_inner_error_train[i,k2] = misclass_rate_test, misclass_rate_train
+             
+        nb_classifier_uni = MultinomialNB(alpha=alpha, fit_prior=False)
+        nb_classifier_uni.fit(dtrainx, dtrainy)
+        y_est_prob_uni = nb_classifier_uni.predict_proba(dtestx)
+        y_est_uni = np.argmax(y_est_prob_uni,1)
+        
+        nb_classifier_est = MultinomialNB(alpha=alpha, fit_prior=True)
+        nb_classifier_est.fit(dtrainx, dtrainy)
+        y_est_prob_est = nb_classifier_est.predict_proba(dtestx)
+        y_est_est = np.argmax(y_est_prob_est,1)
+    
+
+        nb_errors[k2,0] = np.sum(y_est_uni!=dtesty,dtype=float)/dtesty.shape[0]
+        nb_errors[k2,1] = np.sum(y_est_est!=dtesty,dtype=float)/dtesty.shape[0]
+        
+        
         k2+=1
         
-#    f = figure()
-#    plot(tc, inner_error_train.mean(1))
-#    plot(tc, inner_error_test.mean(1))
-#    xlabel('Model complexity (max tree depth)')
-#    ylabel('Error (misclassification rate, CV K={0})'.format(K))
-#    legend(['Error_train','Error_test'])
-    ax[col,row].plot(tc,inner_error_train.mean(1))
-    ax[col,row].plot(tc,inner_error_test.mean(1))
-    ax[col,row].set_title('Loop : {0}'.format(k))
-    row+=1
-    if row == 3:
-        col += 1
-        row = 0
         
-    
 
     
-
-        
+    knn_index = 0;
+    knn_means = np.mean(knn_errors, axis = 0)
+    knn_best = knn_means[0]
+    for j in range(1,L):
+        if knn_means[j] < knn_best:
+            knn_best = knn_means[j]
+            knn_index = j    
+    
     # get the index and lowest error
-    index = 0;
-    means = np.mean(inner_error_test, axis = 1)
-    best = means[0]
+    dtc_index = 0;
+    dtc_means = np.mean(dtc_inner_error_test, axis = 1)
+    dtc_best = dtc_means[0]
     for j in range(1,len(tc)):
-        if means[j] < best:
-            best = means[j]
-            index = j
+        if dtc_means[j] < dtc_best:
+            dtc_best = dtc_means[j]
+            dtc_index = j
+
+     
+    nb_means = np.mean(nb_errors, axis = 0)
+    if nb_means[0] < nb_means[1]:
+        fin_prior = True
+        p = "uniform"
+        z = 0
+    else:
+        fin_prior = False
+        p = "estimate"
+        z = 1
     
+    print('dtc_test_Error of {0} with {1} of {2}'.format(misclass_rate_test,name, tc[dtc_index]))
+    
+
+            
+    print("knn_index : {0} selected as best with error of {1}".format(knn_index, knn_best))
+#    figure()
+#    plot(100*(knn_means))
+#    xlabel('Number of neighbors')
+#    ylabel('Classification error rate (%)')
+#    show()
         
+    print("mb prior selected: {0} with error rate of {1}".format(p, nb_means[z]))
     
-    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth = tc[index])
+    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth = tc[dtc_index])
     dtc = dtc.fit(X_train,y_train.ravel())
     y_est_test = dtc.predict(X_test)
     y_est_train = dtc.predict(X_train)
     # Evaluate misclassification rate over train/test data (in this CV fold)
     misclass_rate_test = sum((y_est_test!=y_test)) / float(len(y_est_test))
     misclass_rate_train = sum((y_est_train!=y_train)) / float(len(y_est_train))
-    Error_test[k], Error_train[k] = misclass_rate_test, misclass_rate_train
-    optimal_depth[k] = tc[index]
-    k+=1
-    print('Error of {0} with {1} of {2}'.format(misclass_rate_test,name, tc[index]))
-   
-f.subplots_adjust(hspace=0.5)   
-show()
+    dtc_Error_test[k], dtc_Error_train[k] = misclass_rate_test, misclass_rate_train
     
-index = 0;
-means = np.mean(Error_test, axis = 1)
-best = means[0]
-for j in range(1,K):
-    if means[j] < best:
-        best = means[j]
-        index = j
+    
+    knclassifier = KNeighborsClassifier(n_neighbors=knn_index+1);
+    knclassifier.fit(X_train, y_train);
+    y_est = knclassifier.predict(X_test);
+    knn_Error_test[k] = np.sum(y_est!=y_test)/len(y_est)
+    
+    
+    nb_classifier_final = MultinomialNB(alpha=alpha, fit_prior=fin_prior)
+    nb_classifier_final.fit(X_train, y_train)
+    y_est_prob_final = nb_classifier_final.predict_proba(X_test)
+    y_est_final = np.argmax(y_est_prob_final,1)
+    
+    
+    NB_Error_test[k] = np.sum(y_est_final!=y_test,dtype=float)/y_test.shape[0]
+    
+#    AllErrors[k,:] = [misclass_rate_test, misclass_rate_train, dtc_index, knn_Error_test[k], knn_index+1, NB_Error_test[k], fin_prior]
+    k+=1
+
+
+#index = 0;
+#means = np.mean(Error_test, axis = 1)
+#best = means[0]
+#for j in range(1,K):
+#    if means[j] < best:
+#        best = means[j]
+#        index = j
         
 
-print('Best error of: {0} at a depth of : {1}'.format(best, optimal_depth[index]))
+#print('Best error of: {0} at a depth of : {1}'.format(best, optimal_depth[index]))
 
 
 
