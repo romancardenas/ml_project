5e693413d893beb92fdf15b2e549093ba330e6c3
Román Cárdenas <rcardenas.rod@gmail.com>
2018-11-07 18:32:14 +0100

    Regression and graphs

--- src/project2_regression.py
+++ src/project2_regression.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
-import neurolab as nl
 from sklearn.model_selection import KFold
 import sklearn.linear_model as lm
 from matplotlib.pyplot import figure, plot, subplot, title, xlabel, ylabel, show, clim, legend
 from toolbox_02450 import feature_selector_lr, bmplot
 from scipy import stats
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras import optimizers
 
 # Read data from CSV file
 #data = pd.read_csv('../data/kc_house_data_clean_regression_nozip.csv')  # Select this if you don't want ZIP codes
@@ -48,12 +51,14 @@ ANN_hidden_units = np.empty((K, 1))         # Number of hidden units (Artificial
 ANN_Error_train = np.empty((K, 1))          # Train error (Artificial Neural Network)
 ANN_Error_test = np.empty((K, 1))           # Test error (Artificial Neural Network)
 
+adam = optimizers.Adam(lr=0.3, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.00001)
+
 #n_hidden_units_test = [2]
 n_hidden_units_test = [4, 6, 8, 10]                # number of hidden units to check (multiplied by the number of inputs)
 n_hidden_units_test = [i * M for i in n_hidden_units_test]
 n_train = 2                                 # number of networks trained in each k-fold
 learning_goal = 100000                      # stop criterion 1 (train mse to be reached)
-max_epochs = 100                            # stop criterion 2 (max epochs in training)
+max_epochs = 1000                           # stop criterion 2 (max epochs in training)
 show_error_freq = 5                         # frequency of training status updates
 
 k = 0
@@ -96,6 +101,7 @@ for train_index, test_index in CV.split(X):  # Outer 2-layer cross-validation lo
     LR_Features_fs[selected_features, k] = 1
     m = lm.LinearRegression(fit_intercept=True).fit(X_train[:, selected_features], y_train)
     LR_Params_fs.append(m.coef_)
+
     LR_Error_train_fs[k] = np.square(y_train - m.predict(X_train[:, selected_features])).sum() / y_train.shape[0]
     LR_Error_test_fs[k] = np.square(y_test - m.predict(X_test[:, selected_features])).sum() / y_test.shape[0]
 
@@ -132,48 +138,67 @@ for train_index, test_index in CV.split(X):  # Outer 2-layer cross-validation lo
 
         bestnet = None
         best_train_error = np.inf
+        best_val_error = np.inf
         bestlayer = None
         for n_hidden_units in n_hidden_units_test:
             print("training with {0} hidden units...".format(n_hidden_units))
             for i in range(n_train):
                 print('Training network {0}/{1}...'.format(i + 1, n_train))
-                # Create randomly initialized network with 2 layers
-                #ann = nl.net.newff([[-3, 8]] * M, [n_hidden_units, 1], [nl.trans.PureLin(), nl.trans.PureLin()])
-                ann = nl.net.newff([[-3, 8]] * M, [n_hidden_units, 1], [nl.trans.TanSig(), nl.trans.PureLin()])
-                # train network
-                train_error = ann.train(X_ANN_train, y_ANN_train.reshape(-1, 1), goal=learning_goal,
-                                        epochs=max_epochs//2, show=show_error_freq)
-                # nl.net.train computes by default SSE.
-                # By multiplying by 2 and dividing by the number samples, we get the MSE
-                train_error = 2 * train_error[-1] / y_ANN_train.shape[0]
+
+                model = Sequential()
+                model.add(Dense(n_hidden_units, input_shape=(M,)))
+                model.add(Activation('relu'))
+                model.add(Dense(1))
+                model.add(Activation('linear'))
+                model.compile(loss='mean_squared_error', optimizer=adam)
+                history = model.fit(X_ANN_train, y_ANN_train, epochs=max_epochs//2, batch_size=X_ANN_train.shape[0],
+                                    verbose=0, validation_data=(X_ANN_test, y_ANN_test))
+
+                train_error = history.history['loss'][-1]
+                val_error = history.history['val_loss'][-1]
+                print('        Training error: {0}'.format(train_error))
                 if train_error < best_train_error:
-                    bestnet = ann.copy()
+                    bestnet = model
                     best_train_error = train_error
+                    best_val_error = val_error
                     bestlayer = n_hidden_units
         print('Best train error: {0} (with {1} hidden layers)'.format(best_train_error, bestlayer))
-        y_ANN_est = bestnet.sim(X_ANN_test).squeeze()
-        inner_fold_error_ANN[inner_k] = np.power(y_ANN_est - y_ANN_test, 2).sum().astype(float) / y_ANN_test.shape[0]
+        print('Best validation error: {0} (with {1} hidden layers)'.format(best_val_error, bestlayer))
+        inner_fold_error_ANN[inner_k] = best_val_error
         n_hidden_units_select[inner_k] = bestlayer
+        #y_ANN_est = bestnet.predict(X_ANN_test).squeeze()
+        #inner_fold_error_ANN[inner_k] = np.power(y_ANN_est - y_ANN_test, 2).sum().astype(float) / y_ANN_test.shape[0]
         inner_k += 1
     best_index = np.asscalar(np.argmin(inner_fold_error_ANN))
     ANN_hidden_units[k] = n_hidden_units_select[best_index]
-    #best_ann = nl.net.newff([[-3, 8]] * M, [int(np.asscalar(n_hidden_units_select[best_index])), 1], [nl.trans.PureLin(), nl.trans.PureLin()])
-    best_ann = nl.net.newff([[-3, 8]] * M, [int(np.asscalar(n_hidden_units_select[best_index])), 1], [nl.trans.TanSig(), nl.trans.PureLin()])
+
     bestnet = None
     best_train_error = np.inf
+    best_val_error = np.inf
     for i in range(n_train):
-        train_error = best_ann.train(X_train, y_train.reshape(-1, 1), goal=learning_goal, epochs=max_epochs,
-                               show=show_error_freq)
-        # nl.net.train computes by default SSE. By multiplying by 2 and dividing by the number samples, we get the MSE
-        train_error = 2 * train_error[-1] / y_train.shape[0]
+        model = Sequential()
+        aux = np.asscalar(n_hidden_units_select[best_index])
+        model.add(Dense(int(np.asscalar(n_hidden_units_select[best_index])), input_shape=(M,)))
+        model.add(Activation('relu'))
+        model.add(Dense(1))
+        model.add(Activation('linear'))
+        model.compile(loss='mean_squared_error', optimizer=adam)
+
+        history = model.fit(X_train, y_train, epochs=max_epochs, batch_size=X_train.shape[0], verbose=0,
+                            validation_data=(X_train, y_train))
+        train_error = history.history['loss'][-1]
+        val_error = history.history['val_loss'][-1]
+        #y_est = model.predict(X_train)
+        #train_error = np.power(y_est - y_train, 2).sum().astype(float) / y_train.shape[0]
+        print('        Training error: {0}'.format(train_error))
         if train_error < best_train_error:
-            bestnet = best_ann.copy()
+            bestnet = model
             best_train_error = train_error
+            best_val_error = val_error
     ANN_Error_train[k] = best_train_error
-    y_est = bestnet.sim(X_test).squeeze()
-    ANN_Error_test[k] = np.power(y_test - y_est, 2).sum().astype(float) / y_test.shape[0]
-    print('Train error: {0}'.format(ANN_Error_train[k]))
-    print('Test error: {0}'.format(ANN_Error_test[k]))
+    ANN_Error_test[k] = best_val_error
+    #y_est = bestnet.predict(X_test).squeeze()
+    #ANN_Error_test[k] = np.power(y_test - y_est, 2).sum().astype(float) / y_test.shape[0]
     k += 1
 
 # Figure with ANN generalization error
